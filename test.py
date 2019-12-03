ef1a4ed3d70ee0162d67bf808fd1e0eba177cfe9
Konstantin Frolov <rnj2000@me.com>
2019-12-03 15:48:08 +0300

    now with path to model file

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from i_want_video import GiveMeVideo
 
 
 vid = GiveMeVideo()
-vid.get_video_recognition()
+vid.get_video_recognition('models_weights\\face_recognition_ep=15_facenet_with_conv.h5')
 #vid.create_face_dataset('DataSet\\test\\chelovek1\\', 'chelovek1')
 
 
